bdaaf45fb9053b2ffb3c037e469d94a729381c63
Kamil Lewucha <lewucha2000@gmail.com>
2023-11-14 20:20:30 +0100

    changed response function from 'send' to 'json'

--- utils/errorHandler.ts
+++ utils/errorHandler.ts
@@ -1,29 +1,35 @@
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, Response } from 'express';
 
 export const errMsg = {
   dataFetch: {
-    EmptyResults: "Cannot find any matching records."
+    EmptyResults: 'Cannot find any matching records.',
   },
   dataInsert: {
-    ObjectAlreadyExistsInDb: "Object already exists in DB.",
-    FailedToInsert: "Cannot insert data."
-  }
-}
+    ObjectAlreadyExistsInDb: 'Object already exists in DB.',
+    FailedToInsert: 'Cannot insert data.',
+  },
+};
 
 export class DataInsertError extends Error {}
 export class FileAccessError extends Error {}
 export class DataFetchError extends Error {}
 
-export const errorHandler = (err: Error, req: Request, res: Response, next:NextFunction) => {
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   switch (err.constructor) {
     case DataFetchError:
-      res.status(404).json({message: err.message})
-    break;
+      res.status(404).json({ message: err.message });
+      break;
     case DataInsertError:
-      res.status(409).json({message: err.message})
-    break;
-    default: 
-      res.status(400).json({message: "An Error Occured. Sorry."})
-    break;
+      res.status(409).json({ message: err.message });
+      break;
+    default:
+      console.error(err.message);
+      res.status(400).json({ message: 'An Error Occured. Sorry.' });
+      break;
   }
 };
